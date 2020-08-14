bc0f1c3f0dac70dc7c841cab35716ff96be8b8fa
Erick <erickrg149@gmail.com>
2020-08-14 17:00:30 -0300

    tentando arrumar login via ldap

--- convida/src/main/java/br/gov/ufpr/convida/resources/JwtAuthenticationController.java
+++ convida/src/main/java/br/gov/ufpr/convida/resources/JwtAuthenticationController.java
@@ -59,7 +59,7 @@ public class JwtAuthenticationController {
                     final UserDetails userDetails = userDetailsService
                             .loadUserByUsername(authenticationRequest.getUsername());
                     final String token = jwtTokenUtil.generateToken(userDetails);
-                    return ResponseEntity.ok(new JwtResponse(token) + idUsuario);
+                    return ResponseEntity.ok(new JwtResponse(token));
 
                 } else {
 
--- convida/src/main/java/br/gov/ufpr/convida/services/JwtUserDetailsService.java
+++ convida/src/main/java/br/gov/ufpr/convida/services/JwtUserDetailsService.java
@@ -21,7 +21,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
 
-        User newUser = user.findById(username).orElse(null);
+        User newUser = user.findByLogin(username);
         if(user == null) {
             throw new UsernameNotFoundException("User not found");
         }else{
